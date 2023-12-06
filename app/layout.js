4d140f958afe8a3fa7ac7df44095d2bf0f263873
Agustin Farias <96506530+fariasagustin3@users.noreply.github.com>
2023-12-06 10:32:27 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import '@/app/ui/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
+  title: 'Agustin Farias Next.js Admin Dashboard',
   description: 'Next.js Tutorial',
 }
 

ed8db9d7abe29b843209acad8e10a77b8f733f47
fariasagustin3 <fariasagustin3@gmail.com>
2023-12-06 10:20:51 -0300

    fix import error at layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from '../ui/dashboard/sidebar/sidebar';
+import Sidebar from '@/app/ui/dashboard/sidebar/Sidebar';
 import Navbar from '../ui/dashboard/navbar/Navbar';
 import styles from '@/app/ui/dashboard/dashboard.module.css';
 import Footer from '../ui/dashboard/footer/Footer';
